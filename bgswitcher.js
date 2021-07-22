01a00803159cf20293612cb459fc32a2d884f813
Keiji Kawaguchi <87131292+jinmati@users.noreply.github.com>
2021-07-22 11:54:40 +0900

    Update bgswitcher.js

--- bgswitcher.js
+++ bgswitcher.js
@@ -1,9 +1,9 @@
 $(window).load(function () {
     $(".bg-slider").bgSwitcher({
       images: [
-        "img/fv-bgi_01@2x.jpg",
-        "img/fv-bgi_02@2x.jpg",
-        "img/fv-bgi_03@2x.jpg",
+        "https://jinmati.github.io/test/fv-bgi_01@2x.jpg",
+        "https://jinmati.github.io/test/fv-bgi_02@2x.jpg",
+        "https://jinmati.github.io/test/fv-bgi_03@2x.jpg",
       ], // 切り替える背景画像を指定
       interval: 5000, // 背景画像を切り替える間隔を指定 3000=3秒
       loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
